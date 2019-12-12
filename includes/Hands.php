0c06a0b7ff417a94e2619335cee34bb069b6883f
Curtis <Curtisnortham@hotmail.com>
2019-12-12 12:39:15 +0000

    Making each hand into an array so we can individualy determine which cards each player has Signed-off-by: Curtis <Curtisnortham@hotmail.com>

--- includes/Hands.php
+++ includes/Hands.php
@@ -6,42 +6,60 @@
 //Suits
 //H-D-S-C
 
-
-
-
 class createHands
 {
     function playerOneHand()
     {
+        //Creates array of possible cards//
         $number = array("2","3","4","5","6","7","8","9","J","Q","K");
         $suit = array("S","C","H","D");
 
-        for($i = 0; $i <= 5; $i++)
+        //Creates first card//
+        $select_number = array_rand($number);
+        $select_suit = array_rand($suit);
+        $card = $number[$select_number].$suit[$select_suit]." ";
+        $hand_array = array($card);
+
+        //Fills rest of hand//
+        while($i != 4 )
         {
             $select_number = array_rand($number);
             $select_suit = array_rand($suit);
             $card = $number[$select_number].$suit[$select_suit]." ";
-            echo $card;
+            array_push($hand_array,$card);
+            $i++;
         }
+        $playerOne = $hand_array[0].$hand_array[1].$hand_array[2].$hand_array[3].$hand_array[4];
+        echo $playerOne;
     }
 
     function playerTwoHand()
     {
+        //Creates array of possible cards//
         $number = array("2","3","4","5","6","7","8","9","J","Q","K");
         $suit = array("S","C","H","D");
 
-        for($i = 0; $i <= 5; $i++)
+        //Creates first card//
+        $select_number = array_rand($number);
+        $select_suit = array_rand($suit);
+        $card = $number[$select_number].$suit[$select_suit]." ";
+        $hand_array = array($card);
+
+        //Fills rest of hand//
+        while($i != 4 )
         {
             $select_number = array_rand($number);
             $select_suit = array_rand($suit);
             $card = $number[$select_number].$suit[$select_suit]." ";
-            echo $card;
+            array_push($hand_array,$card);
+            $i++;
         }
-        
-    }
+        $playerTwo = $hand_array[0].$hand_array[1].$hand_array[2].$hand_array[3].$hand_array[4];
+        echo $playerTwo;
+        }     
 }
 
-$Hand = new createHands();
+
 
 
 
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 include("includes\Hands.php");
-
+include("includes\logic.php");
+$Hand = new createHands();
 
 $Hand->playerOneHand();
 echo "<br>";
